164bc01a4b4b05c717f8e267dab710463b313c2c
kbrzezinska <33931508+kbrzezinska@users.noreply.github.com>
2020-03-25 13:41:43 +0100

    dodanie komentarzy

--- dodawanie/big.s
+++ dodawanie/big.s
@@ -16,22 +16,22 @@ carry: .long 0x0
 
 _start:
 
-movl $3,%edi
-clc
+movl $3,%edi #zaczynamy numeracje od konca, konwencja little endin
+clc   #czyścimy flage przeniesienia
 
 loop:
 
-pushl l1(,%edi,4)
-pushl l2(,%edi,4)
+pushl l1(,%edi,4)    #pierwszy argument funkcji
+pushl l2(,%edi,4)    #drugi argument funkcji
 call add
-pop %eax
-movl (%esp),%eax
-movl %eax,l1(,%edi,4)
+pop %eax              #pozbywamy sie ze stosu ostatniego pusha, czyli l2(,%edi,4) zeby na koncu byl widoczny tylko wynik zapisany w l1
+movl (%esp),%eax      #wynik pierwszego dodawania do eax
+movl %eax,l1(,%edi,4) #eax do czesci l1
 dec %edi
 cmpl $1,%edi
 jge loop
 
-movl carry,%eax
+movl carry,%eax         
 movl %eax,l1(,%edi,4)
 push %eax
 
@@ -45,12 +45,12 @@ push %ebp
 movl %esp, %ebp
 
 movl carry,%eax     #dodanie przeniesienia
-adcl %eax,12(%ebp)
+adcl %eax,12(%ebp)   #dodanie przeniesienia do wyniku, który jest rownoczesnie naszym pierwszym argumentem(l1)
 
-movl 8(%ebp), %eax #pierwszy argument funkcji
-adcl  %eax,12(%ebp)
+movl 8(%ebp), %eax    #drugi argument funkcji(l2) do eax
+adcl  %eax,12(%ebp)   #dodanie pierwszego elementu do drugiego, czyli l1=l1+l2, wynik w 12(%ebp)
 
-setc carry
+setc carry             #ustawiamy przeniesienie w zmiennnej carry, 1- jest, 0- nie ma
 
 
 end:
